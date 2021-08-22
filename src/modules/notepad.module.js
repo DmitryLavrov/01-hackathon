247c395c6eadb58a9bd2242d2ed7b9843b3fcb29
VitaliyOsin <d_dva@list.ru>
2021-08-23 02:02:35 +0700

    23-08-2021 02:02

--- src/modules/notepad.module.js
+++ src/modules/notepad.module.js
@@ -1,137 +1,58 @@
 import { Module } from '../core/module'
-import { qel, cel, setStyle } from '../utils'
+import '../css/notepad.css'
 
 export class NotepadModule extends Module {
   constructor(type, text) {
     super(type, text)
-
     this.num = 'note-72'
-
-    this.sop = {
-        b: '1px solid #ccc',
-        c: '1px solid #ff0000'
-    }
-
-    this.styles = {
-        div: {
-            display: 'inline-block',
-            border: this.sop.b,
-            borderRadius: '5px',
-            marginBottom: '10px',
-            marginRight: '10px',
-            background: 'rgba(255,255,255,0.8)'
-        },
-        tarea: {
-            width: '230px',
-            minWidth: '160px',
-            outline: 'none',
-            height: '230px',
-            margin: '8px',
-            padding: '5px',
-            boxSizing: 'border-box',
-            border: this.sop.b,
-            borderRadius: '4px'
-        },
-        top: {
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            margin: '10px',
-            boxSizing: 'border-box'
-        },
-        h3: {
-            color: '#333',
-            fontWeight: 'bold'
-        },
-        closeBtn: {
-            width: '14px',
-            height: '14px',
-            color: '#fff',
-            background: '#fff',
-            border: this.sop.c,
-            background: '#ff0000',
-            borderRadius: '3px',
-            cursor: 'pointer',
-            fontSize: '11px',
-            fontWeight: 'bold',
-            padding: '2px',
-            lineHight: '20px',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        footerBtn: {
-            border: this.sop.b,
-            borderRadius: '3px',
-            padding: '3px',
-            textAlign: 'center',
-            color: '#333',
-            cursor: 'pointer'
-        },
-        noteList: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            margin: '10px',
-            boxSizing: 'border-box'
-        },
-        p: {
-            background: '#dce',
-            borderRadius: '3px',
-            padding: '3px',
-            textAlign: 'left',
-            width: '100%',
-            marginBottom: '5px'
-        }
-    }
   }
+
   trigger() {
-      this.#create(qel('body'))
+      this.#create(document.querySelector('body'))
   }
 
   #create(point){
-      if(qel('#ntp')) return
-    console.log('NotepadModule')
-    const div = cel('div')
-    div.id = 'ntp'
-    setStyle(div, this.styles.div)
+    if(document.querySelector('#ntp')) return
+    const wrap$ = document.createElement('div')
+    wrap$.id = 'ntp'
+    wrap$.className = 'notepad-wrap '
 
-    const top = cel('div')
-    setStyle(top, this.styles.top)
+    const top$ = document.createElement('div')
+    top$.className = 'notepad-top'
 
-    const h3 = cel('h3')
-    setStyle(h3, this.styles.h3)
-    h3.textContent = 'Блокнот'
+    const title$ = document.createElement('h3')
+    title$.className = 'notepad-title'
+    title$.textContent = 'Блокнот'
 
-    const closeBtn = cel('button')
-    setStyle(closeBtn, this.styles.closeBtn)
-    closeBtn.textContent = 'X'
-    closeBtn.addEventListener('click', () => {this.#close(div)})
+    const closeBtn$ = document.createElement('button')
+    closeBtn$.className = 'notepad-close-btn'
+    closeBtn$.textContent = 'X'
+    closeBtn$.addEventListener('click', () => {this.#close(wrap$)})
     
-    const tarea = cel('textarea')
-    tarea.id = 'tarea'
-    setStyle(tarea, this.styles.tarea)
+    const tarea$ = document.createElement('textarea')
+    tarea$.id = 'tarea'
+    tarea$.className = 'notepad-textarea'
 
-    const footer = cel('div')
-    setStyle(footer, this.styles.top)
+    const footer$ = document.createElement('div')
+    footer$.id = 'notepad-footer'
+    footer$.className = 'notepad-top'
 
-    const openBtn = cel('button')
-    setStyle(openBtn, this.styles.footerBtn)
-    openBtn.textContent = 'Открыть'
-    openBtn.addEventListener('click', this.#open.bind(this, div))
+    const openBtn$ = document.createElement('button')
+    openBtn$.className = 'notepad-footer-btn'
+    openBtn$.textContent = 'Открыть'
+    openBtn$.addEventListener('click', this.#open.bind(this, wrap$))
 
-    const saveBtn = cel('button')
-    setStyle(saveBtn, this.styles.footerBtn)
-    saveBtn.textContent = 'Сохранить'
-    saveBtn.addEventListener('click', this.#save.bind(this, tarea))
+    const saveBtn$ = document.createElement('button')
+    saveBtn$.className = 'notepad-footer-btn'
+    saveBtn$.textContent = 'Сохранить'
+    saveBtn$.addEventListener('click', () => {
+      this.#save(tarea$)
+    })
 
-    footer.append(openBtn, saveBtn)
-    top.append(h3, closeBtn)
-    div.append(top, tarea, footer)
-    point.append(div)
+    footer$.append(openBtn$, saveBtn$)
+    top$.append(title$, closeBtn$)
+    wrap$.append(top$, tarea$, footer$)
+    point.append(wrap$)
   }
 
   #close(el){
@@ -145,62 +66,63 @@ export class NotepadModule extends Module {
       if(note.trim()){
         if(localStorage.getItem(this.num)){                 
             txt = localStorage.getItem(this.num)+'@@'+note
-            console.log(localStorage.getItem(this.num)); 
         }else{
           txt = note
-          console.log(localStorage.getItem(this.num)); 
-        }
-        
+        }       
         localStorage.setItem(this.num, txt)
-      }
-           
-      qel('#tarea').value = ''
-      qel('#tarea').focus()
-      console.log('rtyu');
+      }         
+      document.querySelector('#tarea').value = ''
+      document.querySelector('#tarea').focus()
   }
 
   #open(p){
-      if(qel('#nls')) return
-      const noteList = cel('div')
-      
-        setStyle(noteList, this.styles.noteList)
+      if(document.querySelector('#nls')) return
+      const noteList$ = document.createElement('div')
+      noteList$.className = 'notepad-note-list'
+
       if(localStorage.getItem(this.num)){
+        document.querySelector('#notepad-footer').style.display = 'none'
         const notes = localStorage.getItem(this.num).split('@@') 
-        noteList.id = 'nls'
-        noteList.textContent = ''
+        noteList$.id = 'nls'
+        noteList$.textContent = ''
         if(notes.length > 0){
           notes.forEach(n => {
-              const p = cel('div')
-              setStyle(p, this.styles.p)
-              p.textContent = n
-              noteList.append(p)
+              const p$ = document.createElement('p')
+              p$.className = 'notepad-note-list-p'
+              p$.textContent = n
+              noteList$.append(p$)
           })
         }else{
-          noteList.textContent = 'Записей нет'
+        noteList$.textContent = 'Записей нет'
         }
-        const listFooter = cel('div')
-      setStyle(listFooter, this.styles.top)
 
-      const closeListBtn = cel('bottom')
-      setStyle(closeListBtn, this.styles.footerBtn)
-      closeListBtn.style.fontSize = '11px'
-      closeListBtn.textContent = 'Закрыть список'
-      closeListBtn.addEventListener('click', this.#close.bind(this, noteList))
+      const listFooter$ = document.createElement('div')
+      listFooter$.className = 'notepad-top'
 
-      const clearListBtn = cel('bottom')
-      setStyle(clearListBtn, this.styles.footerBtn)
-      clearListBtn.style.fontSize = '11px'
-      clearListBtn.textContent = 'Очистить список'
-      clearListBtn.addEventListener('click', () => {
-          localStorage.clear()
-          this.#close(noteList)
+      const closeListBtn$ = document.createElement('bottom')
+      closeListBtn$.className = 'notepad-footer-btn'
+      closeListBtn$.textContent = 'Закрыть список'
+      closeListBtn$.addEventListener('click', () => {
+        this.#close(noteList$)
+        document.querySelector('#notepad-footer').style.display = 'flex'
       })
 
-      listFooter.append(closeListBtn, clearListBtn)
-      noteList.append(listFooter)
-      p.append(noteList)
+      const clearListBtn$ = document.createElement('bottom')
+      clearListBtn$.className = 'notepad-footer-btn'
+      clearListBtn$.textContent = 'Очистить список'
+      clearListBtn$.addEventListener('click', () => {
+          localStorage.clear()
+          this.#close(noteList$)
+          document.querySelector('#notepad-footer').style.display = 'flex'
+      })
+
+      listFooter$.append(closeListBtn$, clearListBtn$)
+      noteList$.append(listFooter$)
+      p.append(noteList$)
       }else{
-        alert('Записей пока нет')
+        //alert('Записей пока нет')
+        document.querySelector('#tarea').value = ''
+        document.querySelector('#tarea').value = 'Записей пока нет'
       }
           
   }
