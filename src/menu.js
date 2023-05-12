d8cb87815d168d0c3c667f36bcd4efe46e5e1951
ituzov <ilia@ituzov.ru>
2023-05-12 20:01:33 +0300

    add test module

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,13 @@ import { BackgroundModule } from './modules/background.module';
 export class ContextMenu extends Menu {
 	constructor() {
 		super('#menu');
+		const trig = document.querySelector('.forClick');
+		trig.addEventListener('contextmenu', (event) => {
+			event.preventDefault();
+			this.el.style.top = `${event.clientY}px`;
+			this.el.style.left = `${event.clientX}px`;
+			this.open();
+		});
 	}
 
 	open() {
@@ -15,18 +22,12 @@ export class ContextMenu extends Menu {
 	}
 
 	add() {
-		const trig = document.querySelector('.forClick');
-		trig.addEventListener('contextmenu', (event) => {
-			event.preventDefault();
-			this.el.style.top = `${event.clientY}px`;
-			this.el.style.left = `${event.clientX}px`;
-			this.open();
-		});
 		const bg = new BackgroundModule();
 		this.el.innerHTML = bg.toHTML();
 		this.el.addEventListener('click', (event) => {
-			if (event.target.dataset.type === 'Module') {
+			if (event.target.dataset.type === bg.type) {
 				bg.trigger();
+				this.close();
 			}
 		});
 	}
