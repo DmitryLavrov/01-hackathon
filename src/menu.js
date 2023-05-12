2d5826f8ddd2018cc4ba38553b78230022581d52
Borisov Sergei <BorisovSF.mailbox@gmail.com>
2023-05-12 22:24:53 +0300

    added countdownTimer

--- src/menu.js
+++ src/menu.js
@@ -1,32 +1,44 @@
-import { Menu } from './core/menu';
-import { BackgroundModule } from './modules/background.module';
+import { Menu } from "./core/menu";
+import { BackgroundModule } from "./modules/background.module";
+import { CountdownTimerModule } from "./modules/countdownTimer.module";
 
 export class ContextMenu extends Menu {
 	constructor() {
-		super('#menu');
+		super("#menu");
 	}
 
 	open() {
-		this.el.classList.add('open');
+		this.el.classList.add("open");
 	}
 
 	close() {
-		this.el.classList.remove('open');
+		this.el.classList.remove("open");
 	}
 
 	add() {
-		const trig = document.querySelector('.forClick');
-		trig.addEventListener('contextmenu', (event) => {
+		const trig = document.querySelector(".forClick");
+		trig.addEventListener("contextmenu", (event) => {
 			event.preventDefault();
 			this.el.style.top = `${event.clientY}px`;
 			this.el.style.left = `${event.clientX}px`;
 			this.open();
 		});
+
 		const bg = new BackgroundModule();
-		this.el.innerHTML = bg.toHTML();
-		this.el.addEventListener('click', (event) => {
-			if (event.target.dataset.type === 'Module') {
-				bg.trigger();
+
+		// this.el.innerHTML = bg.toHTML();
+		// this.el.addEventListener("click", (event) => {
+		// 	if (event.target.dataset.type === "Module") {
+		// 		bg.trigger();
+		// 	}
+		// });
+
+		const timer = new CountdownTimerModule();
+
+		this.el.innerHTML = timer.toHTML();
+		this.el.addEventListener("click", (event) => {
+			if (event.target.dataset.type === "CountdownTimer") {
+				timer.trigger();
 			}
 		});
 	}
