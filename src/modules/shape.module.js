45c3b87e3adff7babb8f2421b2aa53e748f2a708
sergey_gaz <sergeytushinsky@i.ua>
2023-05-13 18:52:23 +0300

    released mothods drawRect, drawTriangle, drawCircle

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -12,12 +12,19 @@ export class ShapeModule extends Module {
 		shapeCanvas.id = "shape_";// идентификатор канвы
 		if(shapeCanvas.getContext) {
 			let number = Math.floor(Math.random() * 3);// генерация случайного числа
+			let context = shapeCanvas.getContext("2d");// контекст канвы
 			switch( number ) {
 				case 0:
+					// прямоугольники
+					this.#drawRect(context);
 					break;
 				case 1:
+					// треугольник
+					this.#drawTriangle(context);
 					break;
 				case 2:
+					// круг
+					this.#drawCicle(context);
 					break;
 			}
 			document.body.append(shapeCanvas);
@@ -26,19 +33,23 @@ export class ShapeModule extends Module {
 
 	}
 
-	#drawRect() {
-		let context = shapeCanvas.getContext("2d");// контекст канвы
+	#drawRect(context) {
 		context.fillStyle = "rgb(200, 0, 0)";
 		context.fillRect(10, 10, 55, 50);
 		context.fillStyle = "rgba(0, 0, 200, 0.5)";
 		context.fillRect(30, 30, 55, 50);
 	}
 
-	#drawTriangle() {
-
+	#drawTriangle(context) {
+		context.beginPath();
+		context.moveTo(75, 50);
+		context.lineTo(100, 75);
+		context.lineTo(100, 25);
+		context.fill();
 	}
 
-	#drawCicle() {
-
+	#drawCicle(context) {
+		context.fillStyle = "rgb(200, 0, 0)";
+		context.arc(75, 75, 0, Math.PI * 2, true);
 	}
 }
