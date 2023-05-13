e23d8a3e623775a7a0148585a6270beb7db27d36
Borisov Sergei <BorisovSF.mailbox@gmail.com>
2023-05-13 12:52:23 +0300

    renamed sound files

--- src/modules/randomSound.module.js
+++ src/modules/randomSound.module.js
@@ -12,11 +12,12 @@ export class RandomSoundModule extends Module {
 	iterateFiles() {
 		const pathTosound = "src/static/sounds/";
 		const soundName = "sound";
+		const soundExt = "mp3";
 		const numberOfSounds = 8;
 		const sounds = [];
 
 		for (let i = 0; i < numberOfSounds; i++) {
-			sounds.push(`${pathTosound}${soundName}-00${i + 1}.mp3`);
+			sounds.push(`${pathTosound}${soundName}-${i + 1}.${soundExt}`);
 		}
 
 		const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
