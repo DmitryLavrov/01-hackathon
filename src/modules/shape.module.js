78a950ebac5641d1d4f294ab2c9990868ad0b256
sergey_gaz <sergeytushinsky@i.ua>
2023-05-13 12:02:54 +0300

    override trigger()-method

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,25 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ShapeModule extends Module {
+	constructor() {
+		super("Module", "Shape");
+	}
+
+	trigger() {
+		console.log('Работает');
+		// создаём канву для рисования
+		const shapeCanvas = document.createElement("canvas");
+		shapeCanvas.id = "shape_";// идентификатор канвы
+		if(shapeCanvas.getContext) {
+			let context = shapeCanvas.getContext("2d");// контекст канвы
+			context.fillStyle = "rgb(200, 0, 0)";
+			context.fillRect(10, 10, 55, 50);
+			context.fillStyle = "rgba(0, 0, 200, 0.5)";
+			context.fillRect(30, 30, 55, 50);
+			document.body.append(shapeCanvas);
+		}
+
+
+	}
 
 }
