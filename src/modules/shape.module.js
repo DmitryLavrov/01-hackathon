58fb7e81fb3979f1a519fe8a0c8176a25ca7b972
sergey_gaz <sergeytushinsky@i.ua>
2023-05-13 13:41:06 +0300

    add methods drawRect, drawTriangle,drawCicle

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -11,15 +11,34 @@ export class ShapeModule extends Module {
 		const shapeCanvas = document.createElement("canvas");
 		shapeCanvas.id = "shape_";// идентификатор канвы
 		if(shapeCanvas.getContext) {
-			let context = shapeCanvas.getContext("2d");// контекст канвы
-			context.fillStyle = "rgb(200, 0, 0)";
-			context.fillRect(10, 10, 55, 50);
-			context.fillStyle = "rgba(0, 0, 200, 0.5)";
-			context.fillRect(30, 30, 55, 50);
+			let number = Math.floor(Math.random() * 3);// генерация случайного числа
+			switch( number ) {
+				case 0:
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+			}
 			document.body.append(shapeCanvas);
 		}
 
 
 	}
 
+	#drawRect() {
+		let context = shapeCanvas.getContext("2d");// контекст канвы
+		context.fillStyle = "rgb(200, 0, 0)";
+		context.fillRect(10, 10, 55, 50);
+		context.fillStyle = "rgba(0, 0, 200, 0.5)";
+		context.fillRect(30, 30, 55, 50);
+	}
+
+	#drawTriangle() {
+
+	}
+
+	#drawCicle() {
+
+	}
 }
